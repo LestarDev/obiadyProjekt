d646bba2ebf4a8c39d6f79f62cd36ba942eb6a7c
LestarDev <98614691+LestarDev@users.noreply.github.com>
2022-11-24 08:52:34 +0100

    dziala odznaczanie

--- src/pages/add_obiad/AddObiad.tsx
+++ src/pages/add_obiad/AddObiad.tsx
@@ -6,19 +6,21 @@ const AddObiad = () => {
     const kalendarzRef = useRef<HTMLDivElement>(null);
 
     const odznacz = () => {
-        
+        for(let i=9; i<56; i++){
+            kalendarzRef.current!.children[i].className="";
+        }
     }
 
     const selectColumn = (whichOne: number) => {
         odznacz()
-        for(let i=9; i<56; i+=8){
-            kalendarzRef.current!.children[i].className+=" zaznaczony";
+        for(let i=8+whichOne; i<56; i+=8){
+            kalendarzRef.current!.children[i].className="zaznaczony";
         }
     }
     return (
         <>
             <div className="Kalendarz" ref={kalendarzRef}>
-                <div className="Kalendarz-blank"></div> <div onClick={(e: React.MouseEvent)=>{selectColumn(1)}}><p>PON</p></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div>
+                <div className="Kalendarz-blank"></div> <div onClick={(e: React.MouseEvent)=>{selectColumn(1)}}><p>PON</p></div> <div onClick={(e: React.MouseEvent)=>{selectColumn(2)}}><p>Wt</p></div> <div></div> <div></div> <div></div> <div></div> <div></div>
                 <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div>
                 <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div>
                 <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div>
